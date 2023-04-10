fc245be52f8c4d02cbb098d1290300d0f032578e
Cedric Benonga <cedricbenonga13@gmail.com>
2023-04-10 04:27:43 +0200

    Initial Commit

---
+++ main.py
@@ -0,0 +1,336 @@
+from flask import Flask, render_template, redirect, url_for, flash, request, abort
+from flask_bootstrap import Bootstrap5
+
+# in case Bootstrap5 gives red, please run these two lines of code to the terminal:
+# pip uninstall flask-bootstrap bootstrap-flask
+# pip install bootstrap-flask
+# And, in the interpreter under settings, uninstall both then install bootstrap-flask
+
+from flask_ckeditor import CKEditor
+from datetime import date
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+from forms import CreatePostForm, CommentForm
+from flask_gravatar import Gravatar
+from forms import CreatePostForm, RegisterForm, LoginForm
+from functools import wraps
+import smtplib
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+ckeditor = CKEditor(app)
+Bootstrap5(app)
+
+# Adding avatar profile images for users (for comments)
+gravatar = Gravatar(
+    app, size=100,
+    rating='g',
+    default='wavatar',
+    force_default=False,
+    force_lower=False,
+    use_ssl=False,
+    base_url=None
+)
+
+# CONNECT TO DB
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blogs.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+##CONFIGURE TABLES
+
+class BlogPost(db.Model):
+    __tablename__ = "blog_posts"
+    id = db.Column(db.Integer, primary_key=True)
+
+    # Creating Foreign Key, "users.id" the users refers to the tablename of User.
+    author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    # Creating reference to the User object, the "posts" refers to the posts protperty in the User class.
+    author = relationship("User", back_populates="posts")
+
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    subtitle = db.Column(db.String(250), nullable=False)
+    date = db.Column(db.String(250), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    img_url = db.Column(db.String(250), nullable=False)
+    # ***************Parent Relationship*************#
+    comments = relationship("Comment", back_populates="parent_post")
+
+
+with app.app_context():
+    db.create_all()
+
+
+# Creating an authorization by using login manager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+# Create the User Table
+class User(UserMixin, db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(100))
+    # This below code will act like a List of BlogPost objects attached to each User.
+    # The "author" refers to the author property in the BlogPost class.
+    posts = relationship("BlogPost", back_populates="author")
+    # *******Add parent relationship*******#
+    # "comment_author" refers to the comment_author property in the Comment class.
+    comments = relationship("Comment", back_populates="comment_author")
+
+
+# Line below only required once, when creating DB.
+with app.app_context():
+    db.create_all()
+
+
+class Comment(db.Model):
+    __tablename__ = "comments"
+    id = db.Column(db.Integer, primary_key=True)
+    # text = db.Column(db.Text, nullable=False)
+    # *******Add child relationship*******#
+    # "users.id" The users refers to the tablename of the Users class.
+    # "comments" refers to the comments property in the User class.
+    author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    comment_author = relationship("User", back_populates="comments")
+
+    # ***************Child Relationship*************#
+    post_id = db.Column(db.Integer, db.ForeignKey("blog_posts.id"))
+    parent_post = relationship("BlogPost", back_populates="comments")
+    text = db.Column(db.Text, nullable=False)
+
+
+with app.app_context():
+    db.create_all()
+
+
+# Creating "admin-only" decorator that we will use in edit-post, delete and new-post routes,
+# and make those routes or url(s) only accessible by the admin
+def admin_only(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        # If id is not 1 then return abort with 403 error
+        if current_user.id != 1:
+            return abort(403)
+        # Otherwise continue with the route function
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+@app.route('/')
+def get_all_posts():
+    posts = BlogPost.query.all()
+    return render_template("index.html", all_posts=posts)
+
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit() and request.method == "POST":
+
+        # Checking if the user already exists
+        if User.query.filter_by(email=request.form.get('email')).first():
+            flash("You've already signed up with that email, log in instead!")  # to see this message, you need to add
+            # some lines of code in the login.htl right on top of the form.
+            return redirect(url_for('login'))
+
+        # Hashing and salting the password:
+        hashed_and_salted_password = generate_password_hash(
+            request.form.get('password'),
+            method='pbkdf2:sha256',
+            salt_length=8
+        )
+
+        # Adding new user
+        new_user = User(
+            email=request.form.get('email'),
+            name=request.form.get('name'),
+            password=hashed_and_salted_password
+        )
+
+        print(new_user)
+
+        # Saving the new user in the database
+        db.session.add(new_user)
+        db.session.commit()
+
+        # Login and authenticate user after adding details to database.
+        login_user(new_user)
+        return redirect(url_for("get_all_posts", name=new_user.name))
+
+    return render_template("register.html", form=form)
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit() and request.method == "POST":
+
+        # Get data entered by the user
+        email = request.form.get('email')  # or form.email.data
+        password = request.form.get('password')  # or form.password.data
+
+        # Find user in the DB by using the email entered.
+        user = User.query.filter_by(email=email).first()
+
+        # If email doesn't exist
+        if not user:
+            flash("That email does not exist, please try again.")
+            return redirect(url_for('login'))
+
+        # If password incorrect
+        elif not check_password_hash(user.password, password):
+            flash('Password incorrect, please try again.')
+            return redirect(url_for('login'))
+
+        # If email exists in the DB and password correct, authorize access.
+        else:
+            login_user(user)
+            return redirect(url_for('get_all_posts'))
+
+    return render_template("login.html", form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('get_all_posts'))
+
+
+@app.route("/post/<int:post_id>", methods=["GET", "POST"])
+def show_post(post_id):
+    requested_post = BlogPost.query.get(post_id)
+    form = CommentForm()
+
+    if form.validate_on_submit():
+        if not current_user.is_authenticated:
+            flash("You need to login or register before you comment.")
+            return redirect(url_for("login"))
+
+        new_comment = Comment(
+            text=form.body.data,
+            comment_author=current_user,
+            parent_post=requested_post
+        )
+        db.session.add(new_comment)
+        db.session.commit()
+
+    form.body.data = ""
+    comments = Comment.query.all()
+    return render_template("post.html", post=requested_post, form=form, current_user=current_user, all_comments=comments)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact", methods=["POST", "GET"])
+def contact():
+    if request.method == 'POST':
+        name = request.form["name"]
+        email = request.form["email"]
+        phone = request.form["phone"]
+        message = request.form["message"]
+
+        # send an email
+        my_mail = "bicprogramming@gmail.com"
+        my_password = "yderlbqimxvcqcna"  # taken from gmail setting - security - App password ###
+        # You can't use your gmail account password.
+
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=my_mail, password=my_password)
+            connection.sendmail(
+                from_addr=my_mail,
+                to_addrs=my_mail,
+                msg=f"subject:From Blog Website\n\n{name}\n{email}\n{phone}\n{message}"
+            )
+
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
+
+
+@app.route("/new-post", methods=["GET", "POST"])
+# Mark with decorator
+@admin_only
+def add_new_post():
+    form = CreatePostForm()
+    if form.validate_on_submit() and request.method == "POST":
+        new_post = BlogPost(
+            title=form.title.data,
+            subtitle=form.subtitle.data,
+            body=form.body.data,
+            img_url=form.img_url.data,
+            author=current_user,
+            date=date.today().strftime("%B %d, %Y")
+        )
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for("get_all_posts"))
+    return render_template("make-post.html", form=form)
+
+
+@app.route("/edit_post/<int:post_id>", methods=['POST', 'GET'])
+# Mark with decorator
+@admin_only
+def edit_post(post_id):
+    post = BlogPost.query.get(post_id)
+    edit_form = CreatePostForm(
+        title=post.title,
+        subtitle=post.subtitle,
+        img_url=post.img_url,
+        author=post.author,
+        body=post.body
+    )
+    if request.method == "POST" and edit_form.validate_on_submit():
+        post.title = edit_form.title.data
+        post.subtitle = edit_form.subtitle.data
+        post.img_url = edit_form.img_url.data
+        # post.author = edit_form.author.data
+        post.body = edit_form.body.data
+        db.session.commit()
+        return redirect(url_for("show_post", post_id=post.id))
+
+    return render_template("make-post.html", form=edit_form)
+
+
+@app.route("/delete/<int:post_id>")
+# Mark with decorator
+@admin_only
+def delete_post(post_id):
+    post_to_delete = BlogPost.query.get(post_id)
+    db.session.delete(post_to_delete)
+    db.session.commit()
+    return redirect(url_for('get_all_posts'))
+
+
+@app.route("/delete-comment/<int:comment_id>")
+def delete_comment(comment_id):
+    comment_to_delete = Comment.query.filter_by(id=comment_id).first()
+    post_to_return = BlogPost.query.filter_by(id=comment_to_delete.post_id).first()
+    db.session.delete(comment_to_delete)
+    db.session.commit()
+    return redirect(url_for("show_post", post_id=post_to_return.id))
+
+
+@app.route("/confirmation/<int:post_id>")
+def confirm_delete(post_id):
+    # post_id = request.args.get('id')
+    post_to_delete = BlogPost.query.get(post_id)
+    return render_template("confirm_delete.html", post=post_to_delete)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
